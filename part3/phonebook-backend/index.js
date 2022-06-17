66c9d69f6ed24fd49b04c9e01a0cb6d0df066243
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-17 12:51:02 +0300

    ex: 3.1-3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const entries = [
+let entries = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -28,6 +28,57 @@ const entries = [
 
 app.get("/api/persons", (req, res) => {
   res.json(entries);
+});
+
+// PERSONS
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const entry = entries.find((entry) => entry.id === id);
+  if (entry) {
+    res.json(entry);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+
+  const isNameUnique = entries.every((entry) => entry.name !== body.name);
+
+  if (!isNameUnique) {
+    return res.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+
+  const entry = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 100000),
+  };
+
+  entries = [...entries, entry];
+
+  res.json(entry);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  entries = entries.filter((entry) => entry.id !== id);
+  res.status(204).end();
+});
+
+// INFO
+app.get("/api/info", (req, res) => {
+  res.send(`<p>Phonebook has info about ${entries.length} people</p>
+  <p>${new Date()}</p>`);
 });
 
 const PORT = 3001;
