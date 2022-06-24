edafe08e1e2589c05d8e5708a7b66ac77a1a0642
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-24 18:40:18 +0300

    blog-list: add utils - config, middleware, logger

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
+const mongoUrl = config.MONGO_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -33,7 +35,6 @@ app.post("/api/blogs", (req, res) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
---
+++ part4/blog-list/utils/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const { PORT } = process.env;
+const { MONGO_URI } = process.env;
+
+module.exports = {
+  PORT,
+  MONGO_URI,
+};
