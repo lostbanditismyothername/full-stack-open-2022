b4c35ff6b0128a16055f09182b398fcc067a72c4
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-07-27 15:34:21 +0300

    enable users to delete blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import blogService from "../services/blogs";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const handleDelete = async () => {
+    try {
+      console.log(blog.id);
+      const deletedBlog = await blogService.remove(blog.id, blog);
+      alert(`${deletedBlog.title} is removed`);
+    } catch (exception) {
+      alert(exception.message);
+    }
+  };
+
+  return (
+    <div>
+      <p>
+        {blog.title} {blog.author}
+        <button style={{ marginLeft: 10 }} onClick={handleDelete}>
+          delete
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
